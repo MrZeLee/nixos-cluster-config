c6c3acd48777ed58239e6707697d749edf809115
MrZeLee <mrzelee404@gmail.com>
2025-05-16 18:59:25 +0100

    feat(secrets.nix): nest age.secrets.k3s-token under age attribute to improve structure

--- secrets.nix
+++ secrets.nix
@@ -1,10 +1,12 @@
 { ... }:
 
 {
-  age.secrets.k3s-token = {
-    file = ./secrets/k3s-token.age;
-    owner = "root";
-    mode = "0400";
+  age = {
+    secrets.k3s-token = {
+      file = ./secrets/k3s-token.age;
+      owner = "root";
+      mode = "0400";
+    };
     identityPaths = ["/etc/agenix/age-key.txt"];
   };
 }

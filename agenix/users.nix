24365690b67b59a625b2518838b4da73fa907eee
MrZeLee <mrzelee404@gmail.com>
2025-05-16 18:35:14 +0100

    feat(agenix): add multiple users to agenix users.nix file

--- agenix/users.nix
+++ agenix/users.nix
@@ -1,3 +1,14 @@
-{
-  "shared-key" = "age19hphkxnl3hpctzmmvzjvpy9402c2l442z7vthdc46ncjm906tyzsj689cu";
+let
+  shared = "age19hphkxnl3hpctzmmvzjvpy9402c2l442z7vthdc46ncjm906tyzsj689cu";
+in {
+  "shared-key" = shared;
+  "raspb0" = shared;
+  "raspb1" = shared;
+  "raspb2" = shared;
+  "raspb3" = shared;
+  "raspb4" = shared;
+  "raspb5" = shared;
+  "raspb6" = shared;
+  "server" = shared;
+  "minipc" = shared;
 }

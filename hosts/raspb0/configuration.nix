a15174bc7a453755d7cd10ca5b859b68bcbcf5f5
MrZeLee <mrzelee404@gmail.com>
2025-09-13 23:26:22 +0100

    fix(configuration.nix): change network interface name from eth0 to end0 to match the actual interface name on the device

--- hosts/raspb0/configuration.nix
+++ hosts/raspb0/configuration.nix
@@ -18,7 +18,7 @@
   networking.hostName = name;
 
   # Optional: if you want to override IP per-host
-  networking.interfaces.eth0.ipv4.addresses = [{
+  networking.interfaces.end0.ipv4.addresses = [{
     address = "192.168.2.100";
     prefixLength = 23;
   }];

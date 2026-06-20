20f21b808cd779012d5300172fcdfaabcb3b22a1
MrZeLee <mrzelee@proton.me>
2026-06-20 16:56:20 +0100

    feat(n5pro): mount ZFS zvol as ext4 for Longhorn
    ZFS datasets don't support fallocate(), causing Longhorn replica
    creation to fail. Use a sparse zvol (tank/longhorn-block) formatted
    with ext4, mounted at /mnt/longhorn-hdd-ext4, as the Longhorn disk
    instead. Pool is still passphrase-encrypted; mount is noauto/nofail.

--- hosts/n5pro/zfs-pool.nix
+++ hosts/n5pro/zfs-pool.nix
@@ -4,4 +4,18 @@ _: {
   #   sudo zpool import tank
   #   sudo zfs load-key tank
   #   sudo zfs mount -a
+  #   sudo mount /mnt/longhorn-hdd-ext4
+  #
+  # One-time zvol provisioning (already done):
+  #   sudo zfs create -V 20T tank/longhorn-block
+  #   sudo mkfs.ext4 /dev/zvol/tank/longhorn-block
+
+  fileSystems."/mnt/longhorn-hdd-ext4" = {
+    device = "/dev/zvol/tank/longhorn-block";
+    fsType = "ext4";
+    options = [
+      "nofail"
+      "noauto"
+    ];
+  };
 }
